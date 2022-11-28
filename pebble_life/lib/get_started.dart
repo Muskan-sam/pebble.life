c423ef1e14ae9af814585c7c2f77d028ee885f3d
Muskan Samantaray <samantaray.muskan19@gmail.com>
2022-11-29 00:17:46 +0530

    New Changes updated
    Get started page built successfully and a demo Sign up dart file added.

--- pebble_life/lib/get_started.dart
+++ pebble_life/lib/get_started.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+import 'Registeration.dart';
+
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
 
@@ -9,6 +11,7 @@ class Home extends StatelessWidget {
     return Scaffold(
       body: Container(
         width: double.infinity,
+        height: double.infinity,
         decoration: BoxDecoration(
           color: Colors.white,
         ),
@@ -34,6 +37,7 @@ class Home extends StatelessWidget {
               Image.asset('assets/images/getstarted.png',
               ),
             ),
+            //header text
             Container(
               margin:  EdgeInsets.fromLTRB(0, 0, 0, 25),
               width:  double.infinity,
@@ -44,7 +48,7 @@ class Home extends StatelessWidget {
                   Center(
                     child:
                     Container(
-                      margin:  EdgeInsets.fromLTRB(0, 0, 0, 8),
+                      margin:  EdgeInsets.fromLTRB(0, 0, 0, 2),
                       child:
                       RichText(
                         textAlign: TextAlign.center,
@@ -87,15 +91,148 @@ class Home extends StatelessWidget {
 
                           ]
                         ),
-
-
+                      ),
+                    ),
+                  ),
+                  Center(
+                    child:
+                    Container(
+                      margin:  EdgeInsets.fromLTRB(0, 0, 0, 0),
+                      constraints:  BoxConstraints (
+                        maxWidth:  300,
+                      ),
+                      child:
+                      Text(
+                        'The preservation of Health is easier than the cure of the disease',
+                        textAlign: TextAlign.center,
+                        style: GoogleFonts.poppins(
+                          fontSize:  16,
+                          fontWeight:  FontWeight.w400,
+                          color:  Color(0xff724db9),
+                        ),
                       ),
                     ),
                   )
                 ],
               ),
-            )
+            ),
+            //logo
+            Container(
+              // iconFcq (1:156)
+              margin: EdgeInsets.only(left: 226, bottom: 30),
+                width:  double.infinity,
+                decoration:  BoxDecoration (
+                  borderRadius:  BorderRadius.circular(18),
+                ),
 
+                child:
+                Align(
+                  alignment: Alignment(0.5, 0.6),
+                  child:
+                  Row(
+                    crossAxisAlignment:  CrossAxisAlignment.center,
+                    children:  [
+                      Container(
+                        // rectangle1015Vn5 (1:157)
+                        margin:  EdgeInsets.fromLTRB(0, 0.79, 2.36, 0),
+                        width:  3.94,
+                        height:  15.76,
+                        decoration:  BoxDecoration (
+                          borderRadius:  BorderRadius.circular(18),
+                          color:  Color(0xffe94560),
+                        ),
+                      ),
+                      Container(
+                        // rectangle1016ki1 (1:158)
+                        margin:  EdgeInsets.fromLTRB(0, 0, 2.36, 0),
+                        width:  3.94,
+                        height:  26,
+                        decoration:  BoxDecoration (
+                          borderRadius:  BorderRadius.circular(18),
+                          color:  Color(0xff2e1753),
+                        ),
+                      ),
+                      Container(
+                        // rectangle1017EdB (1:159)
+                        margin:  EdgeInsets.fromLTRB(0, 0.79, 0, 0),
+                        width:  3.94,
+                        height:  9.45,
+                        decoration:  BoxDecoration (
+                          borderRadius:  BorderRadius.circular(18),
+                          color:  Color(0xff2e1753),
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+            ),
+            Container(
+              margin:  EdgeInsets.fromLTRB(56.5, 0, 58.5, 0),
+                child:
+                Center(
+                    child:
+                    SizedBox(
+                      width: 225,
+                      height: 60,
+                      child:
+                      ElevatedButton(
+                        onPressed: () {},
+                        // onPressed: () => Navigator.of(context)
+                        //  .push(MaterialPageRoute(builder: (context) => const Register())),
+                        child: Text('Get Started'),
+                        style: ElevatedButton.styleFrom(
+                          shape: StadiumBorder(),
+                          backgroundColor: Color(0xff74369a),
+                          textStyle: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 25,
+                              fontStyle: FontStyle.normal),
+                        ),
+                      ),
+                    )
+                ),
+            ),
+            Container(
+              margin:  EdgeInsets.only(left: 100),
+              width:  double.infinity,
+              height: double.infinity,
+              child:
+              Row(
+                crossAxisAlignment:  CrossAxisAlignment.center,
+                children:  [
+                  Center(
+                    child:
+                    Container(
+                      margin:  EdgeInsets.fromLTRB(0, 0, 7, 0),
+                      child:
+                      Text(
+                        'Already have an account?',
+                        textAlign:  TextAlign.center,
+                        style:  GoogleFonts.poppins (
+                          fontSize:  16,
+                          fontWeight:  FontWeight.w400,
+                          color:  Color(0xff724db9),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Center(
+                    child:
+                    TextButton(
+                      style: TextButton.styleFrom(
+                        foregroundColor: Color(0xffe94560),
+                      ),
+                      onPressed: () {  },
+                      child: Text('Login',
+                          style: GoogleFonts.poppins(
+                              fontSize: 16,
+                          fontWeight: FontWeight.w700))
+
+                    )
+                  ),
+                ],
+              ),
+            ),
 
           ],
         ),
